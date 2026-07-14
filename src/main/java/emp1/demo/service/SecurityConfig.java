0339c977f1d170da9f9cdf7164956afdb043e7e5
Krishna Vamsi Katta <83060150+Vamsi759@users.noreply.github.com>
2026-07-14 15:44:15 +0530

    Update SecurityConfig.java

--- src/main/java/emp1/demo/service/SecurityConfig.java
+++ src/main/java/emp1/demo/service/SecurityConfig.java
@@ -1,3 +1,10 @@
+package emp1.demo.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 public class SecurityConfig {
 
